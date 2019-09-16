98f9b427fd5fb9603e63bfbcee2d3f067b180cd1
pattyouwehand <pattyouwehand@gmail.com>
2019-09-16 13:55:39 +0200

    setup router

---
+++ auth/router.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const { toJWT, toData } = require('./jwt')
+
+const router = new Router()
+
+
+// define endpoints here, leave empty for now
+
+module.exports = router
