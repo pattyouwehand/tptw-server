dcf820b091bb1bcecc400099330d4188d382d6ad
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-16 14:39:37 +0200

    room model and factory made and added a stream in the index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const db = require('./db')
+const Sse = require('json-sse')
 
+const roomFactory = require('./room/router')
+const Room = require('./room/model')
+
+
+const stream = new Sse()
 const app = express()
 
 const corsMiddleware = cors()
@@ -10,6 +15,30 @@ app.use(corsMiddleware)
 
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
+
+async function serialize () {
+  const rooms = await Room.findAll({})
+
+  return JSON.stringify(rooms)
+} 
+
+async function update () {
+  const data = await serialize()
+  
+  stream.send(data)
+}
+
+async function onStream (req, res) {
+  const data = await serialize()
+
+  stream.updateInit(data)
+  return stream.init(req, res)
+}
+
+app.get('/stream', onStream)
+
+const roomRouter = roomFactory(update)
+app.use(roomRouter)
 
 const port = process.env.PORT || 4000
 
